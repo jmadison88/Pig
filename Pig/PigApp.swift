973bed3cb4a86e82c9bfaf7dee3a115bb309bf56
jmadison <jmad99@sbcglobal.net>
2022-11-09 09:27:30 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Josh Madison on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
